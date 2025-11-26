54946f4c0c176ee06b45d67af60bcadf9f1554c9
BryanStevenliga <bsq.ninasunta@yavirac.edu.ec>
2025-11-26 10:31:53 -0500

    fit

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ HTML_PAGE = """
 </head>
 <body>
     <div class="card">
-        <h1>🚀 Mini IA Flask</h1>
+        <h1>🚀 erick mateo silva suarez</h1>
         <p>
             Bienvenido a tu servicio desplegado con CI/CD.  
             Este proyecto demuestra una API simple con IA simulada.
